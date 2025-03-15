af1d17724417bedec964a8dce69f2346dd45e080
David Groomes <dwgroomes@gmail.com>
2025-03-14 19:42:36 -0500

    Dependency upgrades (it is baffling that the lambda SDKs have not released in so long)

--- lambda/buildSrc/src/main/kotlin/dgroomes.common.gradle.kts
+++ lambda/buildSrc/src/main/kotlin/dgroomes.common.gradle.kts
@@ -2,19 +2,19 @@ plugins {
     java
 }
 
-val slf4jVersion = "2.0.9" // SLF4J releases: http://www.slf4j.org/news.html
-val jacksonVersion = "2.16.0" // Jackson releases: https://github.com/FasterXML/jackson/wiki/Jackson-Releases
+val slf4jVersion = "2.0.17" // SLF4J releases: http://www.slf4j.org/news.html
+val jacksonVersion = "2.18.3" // Jackson releases: https://github.com/FasterXML/jackson/wiki/Jackson-Releases
 val awsLambdaJavaCoreVersion =
     "1.2.2" // AWS Lambda Java releases: https://docs.aws.amazon.com/lambda/latest/dg/java-package.html
 val awsLambdaJavaEventsVersion =
     "3.11.1" // AWS Lambda Java releases: https://docs.aws.amazon.com/lambda/latest/dg/java-package.html
 val awsLambdaJavaLog4j2Version =
     "1.5.1" // AWS Lambda Java releases: https://docs.aws.amazon.com/lambda/latest/dg/java-package.html
-val httpComponentsClientV5Version = "5.3" // HttpComponents v5 releases: https://hc.apache.org/news.html
+val httpComponentsClientV5Version = "5.4.2" // HttpComponents v5 releases: https://hc.apache.org/news.html
 
 
-val junitJupiterVersion = "5.10.1" // JUnit releases: https://junit.org/junit5/docs/current/release-notes/index.html
-val assertJVersion = "3.24.2" // AssertJ releases: https://github.com/assertj/assertj-core/tags
+val junitJupiterVersion = "5.12.1" // JUnit releases: https://junit.org/junit5/docs/current/release-notes/index.html
+val assertJVersion = "3.27.3" // AssertJ releases: https://github.com/assertj/assertj-core/tags
 
 repositories {
     mavenCentral()
@@ -31,8 +31,12 @@ dependencies {
     }
     implementation(platform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testImplementation(platform("org.junit:junit-bom:$junitJupiterVersion"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:$assertJVersion")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks {
--- lambda/echo/build.gradle.kts
+++ lambda/echo/build.gradle.kts
@@ -5,4 +5,7 @@ plugins {
 
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.1")
 }
